a37f629675082897d3f5bada73cfeed62b6f902b
Barrister <charlesawuku2010@gmail.com>
2026-01-08 14:18:42 +0000

    'fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const GA_MEASUREMENT_ID = 'G-DQZ3JPQ1XG';
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}');
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
